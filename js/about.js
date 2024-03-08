5223a1ade5cd76a8b31c441630536b539a18e3a3
Anikey <eagleremlinh@gmail.com>
2024-03-08 21:57:05 +0700

    Perfect

--- js/about.js
+++ js/about.js
@@ -1,11 +1,9 @@
 let Introduceyourself = document.getElementById("Introduceyourself")
 let Careergoals = document.getElementById("Careergoals")
 let Personalproject = document.getElementById("Personalproject")
-let Workexperience = document.getElementById("Workexperience")
 let InYou = document.querySelector(".InYou")
 let CaGo = document.querySelector(".CaGo")
 let PePro = document.querySelector(".PePro")
-let WoEx = document.querySelector(".WoEx")
 const checkIntroduceyourself = () => {
     Object.assign(Introduceyourself.style, {
         display: "block"
@@ -16,13 +14,9 @@ const checkIntroduceyourself = () => {
     Object.assign(Personalproject.style, {
         display: "none"
     });
-    Object.assign(Workexperience.style, {
-        display: "none"
-    });
     InYou.classList.remove("active")
     CaGo.classList.add("active")
     PePro.classList.add("active")
-    WoEx.classList.add("active")
 }
 const checkCareergoals = () => {
     Object.assign(Introduceyourself.style, {
@@ -34,13 +28,9 @@ const checkCareergoals = () => {
     Object.assign(Personalproject.style, {
         display: "none"
     });
-    Object.assign(Workexperience.style, {
-        display: "none"
-    });
     InYou.classList.add("active")
     CaGo.classList.remove("active")
     PePro.classList.add("active")
-    WoEx.classList.add("active")
 }
 const checkPersonalproject = () => {
     Object.assign(Introduceyourself.style, {
@@ -52,29 +42,7 @@ const checkPersonalproject = () => {
     Object.assign(Personalproject.style, {
         display: "block"
     });
-    Object.assign(Workexperience.style, {
-        display: "none"
-    });
     InYou.classList.add("active")
     CaGo.classList.add("active")
     PePro.classList.remove("active")
-    WoEx.classList.add("active")
-}
-const checkWorkexperience = () => {
-    Object.assign(Introduceyourself.style, {
-        display: "none"
-    });
-    Object.assign(Careergoals.style, {
-        display: "none"
-    });
-    Object.assign(Personalproject.style, {
-        display: "none"
-    });
-    Object.assign(Workexperience.style, {
-        display: "block"
-    });
-    InYou.classList.add("active")
-    CaGo.classList.add("active")
-    PePro.classList.add("active")
-    WoEx.classList.remove("active")
 }
